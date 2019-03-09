0e5e6cd52d7f8c609ae3b7fd721561ba6f70d8ba
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-03-09 20:47:20 -0300

    Criar tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Container, Form } from './styles';
 
-const Login = (props) => {
-  const {
-    history: { push },
-  } = props;
-  return (
-    <div>
-      <span>login</span>
-      <button onClick={() => push('/')}>home</button>
-    </div>
-  );
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 };
+class Login extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container>
+        <Card className={classes.card}>
+          <CardContent>
+            <Form>
+              <TextField style={{ width: '100%' }} id="email" label="Email" margin="normal" />
+              <TextField
+                style={{ width: '100%' }}
+                id="password"
+                label="Senha"
+                margin="normal"
+                type="password"
+              />
+              <Button
+                style={{
+                  marginTop: 30,
+                  width: '100%',
+                  backgroundColor: '#43d',
+                  color: '#ffffff',
+                }}
+                variant="contained"
+              >
+                Entrar
+              </Button>
+              <Button
+                style={{
+                  marginTop: 20,
+                  width: '100%',
+                  backgroundColor: '#43d',
+                  color: '#ffffff',
+                }}
+                variant="contained"
+              >
+                Registrar-se
+              </Button>
+            </Form>
+          </CardContent>
+        </Card>
+      </Container>
+    );
+  }
+}
 
-export default Login;
+export default withStyles(styles)(Login);
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  min-height: 100%;
+  width: 100%;
+  background-color: #43d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Form = styled.form`
+  height: 300px;
+  width: 400px;
+`;
